5f1de839e054bc7d8a81e3653db0ef96541328d5
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-06-19 23:07:55 +0500

    Learned about an optimized solution of max subarray sum

---
+++ MaxSubArraySumPrefixSum.java
@@ -0,0 +1,27 @@
+public class MaxSubArraySumPrefixSum {
+    public static void maxSubArraySumPrefix(int[] numbers) {
+        int currSum;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix [] = new int [numbers.length];
+
+        prefix[0] = numbers[0];
+        for (int i=1; i<prefix.length; i++) {
+            prefix[i] = prefix [i-1] + numbers[i];
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i; j < numbers.length; j++) {
+                currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("Max subarray sum: " + maxSum);
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 4, 6, 8, 10};
+        maxSubArraySumPrefix(numbers);
+    }
+}
